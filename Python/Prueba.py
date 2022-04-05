ae6b06e002949d4a25f9de9541cb4f3346d105d5
RubenRuVito <rubenruvito@gmail.com>
2022-04-06 01:19:48 +0200

    Update Prueba.py

--- Python/Prueba.py
+++ Python/Prueba.py
@@ -1,6 +1,5 @@
 # https://programmerclick.com/article/56071642020/
 
-import matplotlib.pyplot as plt
 import seaborn  as sns
 import numpy as np
 import pandas as pd
@@ -11,14 +10,21 @@ from matplotlib.figure import Figure
 
 st.write("My First Streamlit Web App")
 
-iris_data = datasets.load_iris()
+iris_data = sns.datasets.load_iris()
 df = pd.DataFrame(iris_data)
 st.write(df).head()
 
-st.subheader("Iris - Sepal Length")
+#st.subheader("Iris - Sepal Length")
+#fig = Figure()
+#ax = fig.subplots()
+#sns.histplot(pd.to_numeric(df['Sepal.length'], errors='coerce').dropna().astype(np.int64), ax=ax, kde=True)
+#ax.set_xlabel('Sepal Length')
+#ax.set_ylabel('Density')
+#st.pyplot(fig)
+
+#sns.pairplot(iris)
+#plt.show()
+
 fig = Figure()
-ax = fig.subplots()
-sns.histplot(pd.to_numeric(df['Sepal.length'], errors='coerce').dropna().astype(np.int64), ax=ax, kde=True)
-ax.set_xlabel('Sepal Length')
-ax.set_ylabel('Density')
+sns.pairplot(df)
 st.pyplot(fig)
