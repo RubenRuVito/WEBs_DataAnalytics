43249c5a7227231d1360000874cae3b62f806b1c
RubenRuVito <rubenruvito@gmail.com>
2022-04-10 17:26:01 +0200

    Update NBA_Analytics_01.py

--- Python/NBA_Analytics_01.py
+++ Python/NBA_Analytics_01.py
@@ -18,7 +18,7 @@ import seaborn as sns
 from PIL import Image,ImageFilter,ImageEnhance
 
 # Api Data NBA
-from basketball_reference_scraper.teams import * as brs_teams
+from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_roster_stats, get_team_misc
 
 def main():
     st.title("NBA EDA App")
@@ -39,7 +39,7 @@ def main():
     def data_teams_misc(year):
     	#df = pd.read_csv(os.path.join(dataset))
         #df = sns.data_load('iris')
-        df_teams = brs_teams.get_team_misc('',year)
+        df_teams = get_team_misc('',year)
         return pd.DataFrame(df_teams)
     	
 
