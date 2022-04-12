360293344216bdeeb0cc875dcb25041d24e73725
RubenRuVito <rubenruvito@gmail.com>
2022-04-12 16:14:22 +0200

    Update NBA_Analytics_01.py

--- Python/NBA_Analytics_01.py
+++ Python/NBA_Analytics_01.py
@@ -170,7 +170,7 @@ def main():
         with sns.axes_style("white"):
             f, ax = plt.subplots(figsize=(7, 5))
             ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-        st.pyplot()
+            st.pyplot(f)
 
       
     # Show Plots
