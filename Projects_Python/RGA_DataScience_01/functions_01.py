3f46675746f3d6898d905d929f812ad3eb4312de
RubenRuVito <rubenruvito@gmail.com>
2022-07-01 23:38:36 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -3,3 +3,5 @@ import streamlit as st
 def players_eda():
   st.markdown("ESTADISTICAS POR JUGADORES - Temporada Regular (21/22).")
   st.write(st.session_state.df_players)
+  
+  mod_lr = linear_model.LinnearReggresion(fit_intercept=True)
