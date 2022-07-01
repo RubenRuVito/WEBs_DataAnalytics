30114fbe704b7680ca397f52b3c12b0b176d44fe
RubenRuVito <rubenruvito@gmail.com>
2022-07-02 01:39:05 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -1,5 +1,6 @@
 import streamlit as st
-#import seaborn as sns
+import seaborn as sns
+import plotly.graph_objects as plygo
 #from matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
@@ -11,7 +12,11 @@ def players_eda():
   
   #fig = plt.figure(figsize=(10, 4))
   st.write(sns.scatterplot(data=st.session_state.df_players, x=st.session_state.df_players['MP'], y=st.session_state.df_players['PTS'],
-                           hue=st.session_state.df_players['Player']))
+                           hue=st.session_state.df_players['Player']), legend=False)
   st.pyplot()
+  
+  #fig = pgo.Figure(data=pgo.Scatter(x=[1,2,3,4], y=[10,11,12,13], mode='markers', marker=dict(size=[40,60,80,100],
+  #                                                                                             color=[1,2,3,4])))
+  #st.plotly_chart(fig)
   
   mod_lr = LinearRegression(fit_intercept=True)
