2508dc5494f4923282728789b1baa85c04fc1e8c
RubenRuVito <rubenruvito@gmail.com>
2022-07-02 01:49:53 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -4,6 +4,8 @@ import plotly.graph_objects as plygo
 #from matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
+st.set_option('deprecation.showPyplotGlobalUse', False) # Para que no visualice los Warning de Visualización de graficos..
+
 def players_eda():
   st.markdown("ESTADISTICAS POR JUGADORES - Temporada Regular (21/22).")
   st.write(st.session_state.df_players)
@@ -12,11 +14,11 @@ def players_eda():
   
   #fig = plt.figure(figsize=(10, 4))
   st.write(sns.scatterplot(data=st.session_state.df_players, x=st.session_state.df_players['MP'], y=st.session_state.df_players['PTS'],
-                           hue=st.session_state.df_players['Player']), legend=False)
+                           hue=st.session_state.df_players['Player']), legend="False")
   st.pyplot()
   
-  #fig = pgo.Figure(data=pgo.Scatter(x=[1,2,3,4], y=[10,11,12,13], mode='markers', marker=dict(size=[40,60,80,100],
-  #                                                                                             color=[1,2,3,4])))
-  #st.plotly_chart(fig)
+  fig = plypgo.Figure(data=plygo.scatter(x=st.session_state.df_players['MP'], y=st.session_state.df_players['PTS'], mode='markers',
+                                         marker_color=st.session_state.df_players['Player'], text=st.session_state.df_players['Player'])
+  st.plotly_chart(fig)
   
   mod_lr = LinearRegression(fit_intercept=True)
