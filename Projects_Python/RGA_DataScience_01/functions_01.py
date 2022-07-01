22225a7eb77510d416bd443f9f0d0921687999a8
RubenRuVito <rubenruvito@gmail.com>
2022-07-01 23:55:10 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from sklearn.linear_model import LinnearRegression
 
 def players_eda():
   st.markdown("ESTADISTICAS POR JUGADORES - Temporada Regular (21/22).")
   st.write(st.session_state.df_players)
   
-  mod_lr = linear_model.LinnearReggresion(fit_intercept=True)
+  st.line_chart(st.session_state.df_players['PTS'])
+  
+  mod_lr = LinnearReggresion(fit_intercept=True)
