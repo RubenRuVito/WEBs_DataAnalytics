19b272a71309663b072e2ae48794e7ed14848c5c
RubenRuVito <rubenruvito@gmail.com>
2022-07-01 19:32:44 +0200

    Update home.py

--- Projects_Python/RGA_DataScience_01/home.py
+++ Projects_Python/RGA_DataScience_01/home.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         
     # Recuperando los datos de players "totals","Advanced" para la temporada actual, y unir la col "PER" a tabla "totals"
     
-    if 'players' not in st.session_state:
+    if 'df_players' not in st.session_state:
         BASE_URL = 'https://www.basketball-reference.com/'
         STAT_TYPES = ['per_game', 'totals', 'per_minute', 'advanced', 'per_poss', 'play-by-play', 'advanced_box_score']
         ADVANCED_BOX_SCORE_COLS = ['Player','Pos','Tm','Scoring Rate','Efficiency(TS%)','Spacing','Creation','Offensive Load']
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         df_players_advanced = get_players_stats(2022,'advanced')
         
         df_players = pd.concat(df_players_totals, df_players_advanced['PER'])
-        st.session_state.players = df_players
+        st.session_state.df_players = df_players
     
     page = st.sidebar.selectbox(
         "Navegador de paginas:",
