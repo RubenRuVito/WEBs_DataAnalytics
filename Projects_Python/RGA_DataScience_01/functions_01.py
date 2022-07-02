0927162667974d25d9a1825ccb69c98981fbf4dd
RubenRuVito <rubenruvito@gmail.com>
2022-07-02 02:25:02 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -19,7 +19,10 @@ def players_eda():
   
   fig = plygo.Figure(data=plygo.Scatter(x=st.session_state.df_players['MP'], y=st.session_state.df_players['PTS'], mode='markers', \
                                         marker_color=st.session_state.df_players['Player'].index, text=st.session_state.df_players['Player']))
-  fig.update_layout(width=1200, height=500)
+  fig.update_layout(width=1200, height=500, title='Relación Puntos y Minutos x Partido por Jugadores)
+  fig.update_traces(textposition="bottom right") # Para poner el Parametro "text" fijo en la grafica
+  fig.update_xaxes(title_text='MP - Minutos x Partido (Media)')
+  fig.update_yaxes(title_text='PTS - Puntos Temp.Regular')
   st.plotly_chart(fig)
   
   mod_lr = LinearRegression(fit_intercept=True)
