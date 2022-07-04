1f6543ca880c5b42ad5e4f321a441e427d730c2b
RubenRuVito <rubenruvito@gmail.com>
2022-07-04 12:51:01 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -13,8 +13,8 @@ def players_eda():
   
   st.line_chart(st.session_state.df_players['PTS'])
   
-  # category:N
-  # plot_line_chart = alt.Chart(st.session_state.df_players).mark_line(interpolate='basis').encode(alt.X('Player', title='Players'), \
+  st.write(st.session_state.df_players.loc[:,['Player','PTS'])
+  # plot_line_chart = alt.Chart(st.session_state.df_players.loc[:,[).mark_line(interpolate='basis').encode(alt.X('Player', title='Players'), \
   #                                                                                               alt.Y('PTS', title='Puntos (Temp.Regular)'), color='parameter'). \
   # properties(title='Puntos Juagadores Temporada Regular', width=1200, height=500)
 
