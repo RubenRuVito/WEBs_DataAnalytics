7fefc4e90b0d26283f94d4686519d06eae587d75
RubenRuVito <rubenruvito@gmail.com>
2022-07-17 19:57:01 +0200

    Update NBA_Web_Stats_01.py

--- Python/NBA_Web_Stats_01.py
+++ Python/NBA_Web_Stats_01.py
@@ -27,8 +27,16 @@ def main():
          my_bar.progress(percent_complete + 1)
     
     with st.spinner('Wait for it...'):
-        time.sleep(5)
+        time.sleep(3)
     #st.success('Done!')
+    
+    with st.spinner('Wait for it2...'):
+        #time.sleep(5)
+        my_bar = st.progress(0)
+
+        for percent_complete in range(50):
+             time.sleep(0.1)
+             my_bar.progress(percent_complete + 1)
 
 
 def teams_stats():
