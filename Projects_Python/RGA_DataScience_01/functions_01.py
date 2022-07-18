794959a36efa87538c29af7809af90049c25d651
RubenRuVito <rubenruvito@gmail.com>
2022-07-18 14:41:35 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -70,6 +70,31 @@ def teams_eda():
   fig.update_layout(width=1200, height=500, font=dict(size=15), title='Puntuaciones por partido Temp.Regular(2021-22)')
   st.plotly_chart(fig)
   
+  # Gráfica de Radar con 2 Equipos
+  
+  # Preparar 2 muestras(filas de datos con en PlayerName en el index del DF) xa incluir en el metodo de grafica..
+  df_teams_radar = df_games_full.iloc[3:5,(3,7,8,9,10)]
+
+  # df_teams_radar.columns
+  # df_teams_radar.index.values
+  # df_teams_radar.iloc[0].values # Retorna los valores de cada atributo/columna de la fila con indice "n"
+
+  fig = pgo.Figure()
+
+  for ind in range(len(df_teams_radar.index.values)):
+      print(ind)
+      fig.add_trace(pgo.Scatterpolar(r=df_teams_radar.iloc[ind].values,
+                                      theta=df_teams_radar.columns,
+                                      fill='toself',
+                                      #name="TypeWine-%s"%datasets.load_wine().target_names[ind],
+                                      name="Team-%s"%df_teams_radar.index[ind], # Recupera los rownames del dataframe..
+                                      showlegend=True,)
+                                      )
+
+  fig.update_layout(polar=dict(radialaxis=dict(visible=True, range=[0, 40])),
+                               title="Promedio Puntos por Cuartos y OT")
+  st.plotly_chart(fig)
+  
   # NUevo objet DF agrupado por equipos y utilizar el método de agregación del promedio o media de puntos en cada quarto y OT
   df_games_full_01 = df_games_full.loc[:,['TEAM_ABBREVIATION','PTS_QTR1','PTS_QTR2','PTS_QTR3','PTS_QTR4','PTS_OT1','PTS_OT2','PTS_OT3','PTS']]. \
                                   groupby('TEAM_ABBREVIATION').mean()
