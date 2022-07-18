2c9b33db0eb1064e72435b533d4d43f04fa3cb6f
RubenRuVito <rubenruvito@gmail.com>
2022-07-18 14:45:56 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -73,7 +73,7 @@ def teams_eda():
   # Gráfica de Radar con 2 Equipos
   
   # Preparar 2 muestras(filas de datos con en PlayerName en el index del DF) xa incluir en el metodo de grafica..
-  df_teams_radar = df_games_full.iloc[3:5,[3,7,8,9,10]]
+  df_teams_radar = df_games_full.iloc[3:5,[4,8,9,10,11]] # filas en posición 3 y 4 y columnas "8..11"
 
   # df_teams_radar.columns
   # df_teams_radar.index.values
