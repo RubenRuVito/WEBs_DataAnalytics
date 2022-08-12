e8bda3b92bfaa7a402256082590ba893f0879631
RubenRuVito <rubenruvito@gmail.com>
2022-08-12 14:11:59 +0200

    Update functions_01.py

--- Projects_Python/RGA_DataScience_01/functions_01.py
+++ Projects_Python/RGA_DataScience_01/functions_01.py
@@ -11,6 +11,49 @@ from sklearn.linear_model import LinearRegression
 
 st.set_option('deprecation.showPyplotGlobalUse', False) # Para que no visualice los Warning de Visualización de graficos..
 
+# Función de github, para scrapear las tablas de estadisticas de Players de "basketball_reference.com"
+def get_players_stats(season: int, stat_type: str, header: int = 0, filter_games=True, remove_duplicates=True):
+  
+    BASE_URL = 'https://www.basketball-reference.com/'
+    STAT_TYPES = ['per_game', 'totals', 'per_minute', 'advanced', 'per_poss', 'play-by-play', 'advanced_box_score']
+    ADVANCED_BOX_SCORE_COLS = ['Player','Pos','Tm','Scoring Rate','Efficiency(TS%)','Spacing','Creation','Offensive Load']
+    
+    url = f'{BASE_URL}leagues/NBA_{str(season)}_{stat_type}.html'
+    print(f'GET {url}')
+    st.text(url)
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+
+    raw = None
+    if 'Age' in df:
+        raw = df.drop(df[df.Age == 'Age'].index)
+        raw = raw.fillna(0)
+    
+    player_stats = raw.drop(['Rk'], axis=1) if raw is not None else df.drop(['Rk'])
+
+    cols=[i for i in player_stats.columns if i not in ['Player','Pos', 'Tm']]
+    for col in cols:
+        try:
+            player_stats[col]=pd.to_numeric(player_stats[col])
+        except ValueError:
+            player_stats[col]=player_stats[col]
+    
+    # Se filtran los jugadores con un minimo de partidos jugados..
+    if filter_games:
+        max_games_played = player_stats['G'].max()
+        threshold = max_games_played // 2   
+        player_stats = player_stats[player_stats['G'] >= threshold]
+
+    if remove_duplicates:
+        player_stats.drop_duplicates(subset=['Player'], inplace=True)
+        player_stats['Pos'].replace(['SG-PG','SG-SF','SG-PF','SG-C'], 'SG', inplace=True)        
+        player_stats['Pos'].replace(['PG-SG','PG-SF','PG-PF','PG-C'], 'PG', inplace=True)
+        player_stats['Pos'].replace(['SF-PG','SF-SG','SF-PF','SF-C'], 'SF', inplace=True)
+        player_stats['Pos'].replace(['PF-PG','PF-SF','PF-SF','PF-C'], 'PF', inplace=True)
+        player_stats['Pos'].replace(['C-PG','C-SF','C-PF','C-SG'], 'C', inplace=True)        
+        
+    return player_stats
+
 def players_eda():
   st.markdown("ESTADISTICAS POR JUGADORES - Temporada Regular (21/22).")
   st.write(st.session_state.df_players)
